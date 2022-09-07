fe598dc5ba39622464d0243722c146066e6cf8c0
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-09-07 14:55:38 +0100

    Fixed UICache

--- test/Services/WhenUsingUICacheService.cs
+++ test/Services/WhenUsingUICacheService.cs
@@ -2,10 +2,6 @@
 using FamilyHubs.ServiceDirectoryAdminUi.Ui.Services.Api;
 using FluentAssertions;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,7 +13,7 @@ public class WhenUsingUICacheService : BaseClientService
     public async Task ThenGetUICacheById()
     {
         //Arrange
-        var uiCache = new UICacheDto() { Id = "Id", Value = "Value" };
+        var uiCache = new UICacheDto("Id", "Value");
         var json = JsonConvert.SerializeObject(uiCache);
         var mockClient = GetMockClient(json);
         UICacheService uiCacheService = new(mockClient);
@@ -34,7 +30,7 @@ public class WhenUsingUICacheService : BaseClientService
     public async Task ThenCreateUICache()
     {
         //Arrange
-        var uiCache = new UICacheDto() { Id = "Id", Value = "Value" };
+        var uiCache = new UICacheDto("Id", "Value");
         var mockClient = GetMockClient(uiCache.Id);
         UICacheService uiCacheService = new(mockClient);
 
@@ -50,7 +46,7 @@ public class WhenUsingUICacheService : BaseClientService
     public async Task ThenUpdateUICache()
     {
         //Arrange
-        var uiCache = new UICacheDto() { Id = "Id", Value = "Value1" };
+        var uiCache = new UICacheDto("Id", "Value");
         var mockClient = GetMockClient(uiCache.Id);
         UICacheService uiCacheService = new(mockClient);
 
